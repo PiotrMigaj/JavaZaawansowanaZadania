9e3ecab96338bfaabd836737f83e74dd79b2f618
Piotr Migaj <pmigaj@gmail.com>
2022-06-01 23:30:00 +0200

    next commit

--- src/main/java/pl/migibud/day3/ex31/Main.java
+++ src/main/java/pl/migibud/day3/ex31/Main.java
@@ -1,5 +1,7 @@
 package pl.migibud.day3.ex31;
 
+import java.io.ByteArrayOutputStream;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -12,14 +14,23 @@ public class Main {
     public static void main(String[] args) throws IOException {
 
         Path myPath = Paths.get("src/main/java/pl/migibud/day3/ex31/text.txt");
-        List<String> myContent = Files.readAllLines(myPath);
+//        List<String> myContent = Files.readAllLines(myPath);
+//
+//        StringBuilder stringBuilder = new StringBuilder(myContent.stream().collect(Collectors.joining(" ")));
+//        String[] outPut = stringBuilder.reverse().toString().split(" ");
+//        List<String> myOutPutList = Arrays.asList(outPut);
+//        System.out.println(Arrays.toString(outPut));
+//
+//        Files.write(Paths.get("src/main/java/pl/migibud/day3/ex31/text2.txt"),myOutPutList);
 
-        StringBuilder stringBuilder = new StringBuilder(myContent.stream().collect(Collectors.joining(" ")));
-        String[] outPut = stringBuilder.reverse().toString().split(" ");
-        List<String> myOutPutList = Arrays.asList(outPut);
-        System.out.println(Arrays.toString(outPut));
+        FileWriter fileWriter = new FileWriter("src/main/java/pl/migibud/day3/ex31/text3.txt");
+        fileWriter.append("Czesc piotr");
+        fileWriter.append("Czesc piotr");
+        fileWriter.close();
 
-        Files.write(Paths.get("src/main/java/pl/migibud/day3/ex31/text2.txt"),myOutPutList);
+
+
+
 
 
 
