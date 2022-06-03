318d97c7c26b888c88cb7a7be9cfb06c3e46260b
Piotr Migaj praca <piotr.migaj@pg-projekt.pl>
2022-06-03 13:00:16 +0200

    next commit

--- src/main/java/pl/migibud/workbook/codewars/ex6/Main.java
+++ src/main/java/pl/migibud/workbook/codewars/ex6/Main.java
@@ -1,25 +1,64 @@
 package pl.migibud.workbook.codewars.ex6;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
 
-        String preSorting = "IronMan;Thor;Captain America;Hulk\n"
-                + "arrogant;divine;honorably;angry\n"
-                + "armor;hammer;shield;greenhorn\n"
-                + "Tony;Thor;Steven;Bruce";
+        String preSorting = "myjinxin2015;raulbc777;smile67;Dentzil;SteffenVogel_79\n"
+                + "17945;10091;10088;3907;10132\n"
+                + "2;12;13;48;11";
+        String afterSorting = "Dentzil;myjinxin2015;raulbc777;smile67;SteffenVogel_79\n"
+                + "3907;17945;10091;10088;10132\n"
+                + "48;2;12;13;11";
+        System.out.println(afterSorting.equals(sortCsvColumns(preSorting)));
+
+    }
+
+    public static String sortCsvColumns(String csvFileContent)
+    {
+        List<List<String>> inputSuperList = Arrays.stream(csvFileContent.split("\n"))
+                .map(v->Arrays.stream(v.split(";")).collect(Collectors.toList()))
+                .collect(Collectors.toList());
 
 
+        List<String> headList = new ArrayList<>(inputSuperList.get(0));
 
 
-        String result = Arrays.stream(preSorting.split("\n"))
-                .map(v-> Arrays.stream(v.split(";")).sorted(String::compareToIgnoreCase).collect(Collectors.joining(";")))
+        List<List<String>> outPutSuperList = new ArrayList<>();
+
+        for (int i =0;i<inputSuperList.size();i++){
+            List<String> tmpList =getSortedListBasedOnAnotherList(headList,String::compareToIgnoreCase,inputSuperList.get(i));
+            outPutSuperList.add(tmpList);
+        }
+
+        String outputString = outPutSuperList.stream()
+                .map(v->v.stream().collect(Collectors.joining(";")))
                 .collect(Collectors.joining("\n"));
 
-        System.out.println(result);
+        return outputString;
+    }
 
+    public static <T,U> List<U> getSortedListBasedOnAnotherList(List<T> inputPatternList,
+                                                                Comparator<T> inputPatternListComp,
+                                                                List<U> listToBeSorted){
+
+        List<T> inputPatternListAfterSorting = inputPatternList.stream()
+                .sorted(inputPatternListComp)
+                .collect(Collectors.toList());
+        int[] newIndexes = new int[inputPatternList.size()];
+        for (int i=0;i< newIndexes.length;i++){
+            T tmpT = inputPatternListAfterSorting.get(i);
+            newIndexes[i] = inputPatternList.indexOf(tmpT);
+        }
+        List<U> listToBeSortedAfterSort = new ArrayList<>();
+        for (int i=0;i< newIndexes.length;i++){
+            listToBeSortedAfterSort.add(listToBeSorted.get(newIndexes[i]));
+        }
+        return listToBeSortedAfterSort;
     }
 }
--- src/main/java/pl/migibud/workbook/codewars/ex6/TestMainSortowanko.java
+++ src/main/java/pl/migibud/workbook/codewars/ex6/TestMainSortowanko.java
@@ -2,6 +2,7 @@ package pl.migibud.workbook.codewars.ex6;
 
 import java.lang.reflect.Array;
 import java.util.*;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -10,47 +11,73 @@ public class TestMainSortowanko {
 
         List<String> names = new ArrayList<>(List.of("Piotr","Ania","Basia"));
         List<String> role = new ArrayList<>(List.of("father","mother","daughter"));
+        List<Integer> age = new ArrayList<>(List.of(30,28,1));
 
-        List<String> namesAfterSorting = names.stream().sorted().collect(Collectors.toList());
-        System.out.println(namesAfterSorting);
-
-
-        int[] namesNewOrder = new int[names.size()];
-
-        for (int i=0;i< namesNewOrder.length;i++){
-            String name = namesAfterSorting.get(i);
-            namesNewOrder[i] = names.indexOf(name);
-        }
-
-        System.out.println(Arrays.toString(namesNewOrder));
-
-        List<String> sortedRole = new ArrayList<>();
-
-        for (int i=0;i< namesNewOrder.length;i++){
-            sortedRole.add(role.get(namesNewOrder[i]));
-        }
-
-        System.out.println(sortedRole);
+        role = getSortedListBasedOnAnotherList(names,String::compareTo,role);
+        age = getSortedListBasedOnAnotherList(names,String::compareTo,age);
+        names.sort(String::compareTo);
+        System.out.println(names);
+        System.out.println(role);
+        System.out.println(age);
 
 
+//        List<String> namesAfterSorting = names.stream().sorted().collect(Collectors.toList());
+//        System.out.println(namesAfterSorting);
 
-//        System.out.println(names);
+
+//        int[] namesNewOrder = new int[names.size()];
 //
-//        Map<String,Integer> familyNames = new HashMap<>();
-//        for (int i=0;i<names.size();i++){
-//            String name = names.get(i);
-//            familyNames.put(name,i);
+//        for (int i=0;i< namesNewOrder.length;i++){
+//            String name = namesAfterSorting.get(i);
+//            namesNewOrder[i] = names.indexOf(name);
 //        }
 //
-//        System.out.println(familyNames);
+//        System.out.println(Arrays.toString(namesNewOrder));
 //
-//        role.sort(new NamesComparator(familyNames));
+//        List<String> sortedRole = new ArrayList<>();
+//
+//        for (int i=0;i< namesNewOrder.length;i++){
+//            sortedRole.add(role.get(namesNewOrder[i]));
+//        }
+//
+//        System.out.println(sortedRole);
+//
+//        List<Integer> myMap = names.stream()
+//                .collect(Collectors.toMap(Function.identity(),v->names.indexOf(v),(v1,v2)->v1,TreeMap::new))
+//                .entrySet().stream()
+//                .map(v->v.getValue())
+//                .collect(Collectors.toList());
+//
+//        System.out.println(myMap);
+//
+//        Comparator<String> myNewComparator = new Comparator<String>() {
+//            @Override
+//            public int compare(String o1, String o2) {
+//                return myMap.get(role.indexOf(o2))- myMap.get(role.indexOf(o1));
+//            }
+//        };
+//
+//        System.out.println();
+//        role.sort(myNewComparator);
 //        System.out.println(role);
+    }
 
+    public static <T,U> List<U> getSortedListBasedOnAnotherList(List<T> inputPatternList,
+                                                              Comparator<T> inputPatternListComp,
+                                                              List<U> listToBeSorted){
 
-
-
-
-
+        List<T> inputPatternListAfterSorting = inputPatternList.stream()
+                .sorted(inputPatternListComp)
+                .collect(Collectors.toList());
+        int[] newIndexes = new int[inputPatternList.size()];
+        for (int i=0;i< newIndexes.length;i++){
+            T tmpT = inputPatternListAfterSorting.get(i);
+            newIndexes[i] = inputPatternList.indexOf(tmpT);
+        }
+        List<U> listToBeSortedAfterSort = new ArrayList<>();
+        for (int i=0;i< newIndexes.length;i++){
+            listToBeSortedAfterSort.add(listToBeSorted.get(newIndexes[i]));
+        }
+        return listToBeSortedAfterSort;
     }
 }
