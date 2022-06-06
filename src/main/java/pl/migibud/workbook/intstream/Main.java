bd688b93c58d8df91c925146a7c2473e33e81987
Piotr Migaj praca <piotr.migaj@pg-projekt.pl>
2022-06-06 16:43:29 +0200

    next commit

--- src/main/java/pl/migibud/workbook/intstream/Main.java
+++ src/main/java/pl/migibud/workbook/intstream/Main.java
@@ -1,14 +1,25 @@
 package pl.migibud.workbook.intstream;
 
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.stream.IntStream;
 
 public class Main {
     public static void main(String[] args) {
-        int n=42;
-         IntStream.range(0,n)
-                .map(i->(n*(n-1)+1)+2*i)
-                .sum();
+//        int n=42;
+//         IntStream.range(0,n)
+//                .map(i->(n*(n-1)+1)+2*i)
+//                .sum();
+
+        List<Integer> myList = new ArrayList<>();
+
+
+
+        IntStream.range(0,10)
+                .forEach(i->myList.add(i));
+
+        System.out.println(myList);
 
     }
 }
--- src/main/java/pl/migibud/workbook/wildcard/Main.java
+++ src/main/java/pl/migibud/workbook/wildcard/Main.java
@@ -1,14 +1,25 @@
 package pl.migibud.workbook.wildcard;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
 
-        List<? extends Number> list = new ArrayList<>();
+//        List<? extends Integer> list = new ArrayList<>();
 //        list.add(32);
-//        list.add(32.2D);
+//        list.add(32.0);
+//
+//        Integer a = list.get(0);
+//
+//        for (Object o:list){
+//            System.out.println(o);
+//        }
+//
+//        var l = list.get(0);
+//        System.out.println(l instanceof Integer);
+
 
     }
 }
