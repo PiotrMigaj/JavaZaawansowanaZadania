eeee8249ad1901f0d8022e27ec00e95c57d4ea04
Piotr Migaj praca <piotr.migaj@pg-projekt.pl>
2022-06-07 09:39:04 +0200

    next commit

--- src/main/java/pl/migibud/generics/animals/Box.java
+++ src/main/java/pl/migibud/generics/animals/Box.java
@@ -1,14 +1,39 @@
 package pl.migibud.generics.animals;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class Box {
 
-//    public T t;
+//    List<?> boxList;
 //
-//    public T getT() {
-//        return t;
+//    public Box(List<? super Number> boxList) {
+//        this.boxList = boxList;
 //    }
-//
-//    public void setT(? extends T) {
-//        this.t = t;
+
+//    public void add(Number n){
+//        boxList.add(n);
 //    }
+
+//    public void print(List<? super Number> input){
+//        for (Number o:input) {
+//            System.out.println(o.toString());
+//        }
+//    }
+
+    public static void main(String[] args) {
+
+        List<? super Number> input = new ArrayList<>();
+
+        input.add(3);
+        input.add(3.2);
+        input.add(3.25);
+
+        for (Object number:input) {
+            System.out.println(number);
+        }
+
+
+    }
 }
