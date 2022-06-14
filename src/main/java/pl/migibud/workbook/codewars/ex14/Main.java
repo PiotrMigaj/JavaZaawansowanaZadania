b5c68a0929b8cac196c46cbdefd16a4e99bd77f1
Piotr Migaj praca <piotr.migaj@pg-projekt.pl>
2022-06-14 12:58:29 +0200

    next commit

---
+++ src/main/java/pl/migibud/workbook/codewars/ex14/Main.java
@@ -0,0 +1,17 @@
+package pl.migibud.workbook.codewars.ex14;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class Main {
+    public static void main(String[] args) {
+
+        String text = "ala";
+
+        List<String> collect = IntStream.range(0, 3).mapToObj(i -> Arrays.stream(text.split("")).map(v -> v.repeat(i+1)).collect(Collectors.joining("-"))).collect(Collectors.toList());
+        System.out.println(collect);
+
+    }
+}
