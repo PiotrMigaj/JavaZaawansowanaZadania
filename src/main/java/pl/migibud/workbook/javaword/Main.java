944d21c428a5d3ec35e333d3af7be1108b7a3457
Piotr Migaj praca <piotr.migaj@pg-projekt.pl>
2022-06-21 14:44:45 +0200

    next commit

--- src/main/java/pl/migibud/workbook/javaword/Main.java
+++ src/main/java/pl/migibud/workbook/javaword/Main.java
@@ -3,6 +3,7 @@ package pl.migibud.workbook.javaword;
 import com.spire.doc.Document;
 import com.spire.doc.FileFormat;
 import com.spire.doc.documents.BookmarksNavigator;
+import com.spire.doc.interfaces.IDocument;
 
 public class Main {
     public static void main(String[] args) {
@@ -11,10 +12,29 @@ public class Main {
         BookmarksNavigator bookmarksNavigator = new BookmarksNavigator(doc);
 
         bookmarksNavigator.moveToBookmark("zakladka");
+        doc.setEmbedFontsInFile(true);
+
+        Document newDoc = new Document();
+        doc.cloneDefaultStyleTo(newDoc);
+        doc.cloneThemesTo(newDoc);
+        doc.cloneCompatibilityTo(newDoc);
+
 
         bookmarksNavigator.replaceBookmarkContent("This is new content", false);
 
+
         doc.saveToFile("src/main/java/pl/migibud/workbook/javaword/ReplaceWithText.docx", FileFormat.Docx);
+
+        Document template = new Document();
+        template.loadFromFile("src/main/java/pl/migibud/workbook/javaword/ReplaceWithText.docx");
+
+        IDocument replaceDoc = new Document();
+        replaceDoc.loadFromFile("src/main/java/pl/migibud/workbook/javaword/ReplaceWithText.docx");
+
+        // Replace specified text with the other document
+        template.replace("${content}", replaceDoc, false, true);
+
+        template.saveToFile("src/main/java/pl/migibud/workbook/javaword/copytest.docx", FileFormat.Docx);
 
 
     }
