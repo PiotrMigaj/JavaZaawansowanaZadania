0d2c3043303138239b6b412878e2442a976ffea8
Piotr Migaj praca <piotr.migaj@pg-projekt.pl>
2022-06-28 08:46:07 +0200

    next commit

--- src/main/java/pl/migibud/workbook/codewars/ex21/Main.java
+++ src/main/java/pl/migibud/workbook/codewars/ex21/Main.java
@@ -1,38 +1,5 @@
 package pl.migibud.workbook.codewars.ex21;
 
-<<<<<<< HEAD
-import java.util.Arrays;
-import java.util.stream.IntStream;
-
-public class Main {
-	public static void main(String[] args) {
-
-		int[] ints = {};
-
-//		int[] ints1 = IntStream.range(0, ints.length).toArray();
-//
-//		System.out.println(Arrays.toString(ints1));
-//
-//		Arrays.sort(ints);
-//
-//		boolean equals = Arrays.equals(ints, ints1);
-//
-//		System.out.println(equals);
-
-		System.out.println(isAllPossibilities(ints));
-
-
-	}
-
-	public static boolean isAllPossibilities(int[] arg) {
-		if (arg.length==0){
-			return false;
-		}
-		Arrays.sort(arg);
-		int[] result = IntStream.range(0, arg.length).toArray();
-		return Arrays.equals(arg, result);
-	}
-=======
 import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
@@ -90,5 +57,4 @@ public class Main {
             System.out.println(iterator.next());
         }
     }
->>>>>>> origin/main
 }
--- src/main/java/pl/migibud/workbook/codewars/ex23/Main.java
+++ src/main/java/pl/migibud/workbook/codewars/ex23/Main.java
@@ -1,5 +1,7 @@
 package pl.migibud.workbook.codewars.ex23;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
 
@@ -7,5 +9,8 @@ public class Main {
 
         System.out.println(number%10);
 
+        String[] split = String.valueOf(number).split("");
+
+        Arrays.toString(split);
     }
 }
